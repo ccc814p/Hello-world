4998c2c25a6d8e02462f18c87d94836d1ba7ab78
ccc814p <63892230+ccc814p@users.noreply.github.com>
2020-08-22 19:12:32 -0400

    Create newpkgr.py

---
+++ newpkgr.py
@@ -0,0 +1,15 @@
+import sys
+pkglist = ['pkgr']
+if argv[1]=="-h":
+  print("pkgr.py -a: Add package\npkgr -h: Get help")
+if argv[1]=="-a":
+  pkgname = str(argv[2])
+  print("Installing "+pkgname+"...")
+  pkglist.append(pkgname)
+if argv[1]=="-r":
+  pkgname = str(argv[2])
+  print("Removing "+pkgname+"...")
+  pkglist.remove(pkgname)
+if argv[1]=="-l":
+  for x in pkglist:
+    print(x+"\n")
